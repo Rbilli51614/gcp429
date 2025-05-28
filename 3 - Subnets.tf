cb2d5f340e26e6f183d2a6225bf65cb46cf1da39
RoyceB <cybertiger51614@gmail.com>
2025-05-27 21:24:34 -0500

    addeded to subnet and updated 7-compute and added 12-LB FE

--- 3 - Subnets.tf
+++ 3 - Subnets.tf
@@ -43,3 +43,15 @@ resource "google_compute_subnetwork" "stark2" {
   network                  = google_compute_network.andor.id
   private_ip_google_access = true
 }
+
+# Regional Proxy-Only Subnet 
+# Required for Regional Application Load Balancer for traffic offloading
+resource "google_compute_subnetwork" "regional_proxy_subnet" {
+  name          = "regional-proxy-subnet"
+  region        = "us-south1"
+  ip_cidr_range = "192.168.10.0/24"
+  # This purpose reserves this subnet for regional Envoy-based load balancers
+  purpose       = "REGIONAL_MANAGED_PROXY"
+  network       = google_compute_network.main.id
+  role          = "ACTIVE"
+}
--- 7a - Compute.tf
+++ 7a - Compute.tf
@@ -19,3 +19,25 @@ resource "google_compute_instance" "sample-vm" {
   metadata_startup_script = file("./startup.sh")
 
 }
+
+resource "google_compute_instance" "public-lannester-1" {
+  name         = "public-lannester-1"
+  machine_type = "e2-medium"
+  zone         = "us-south1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.lannester.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = file("./startup.sh")
+
+}
