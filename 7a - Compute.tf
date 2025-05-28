6f705cf2f448a5e4128913d798152965700e9950
RoyceB <cybertiger51614@gmail.com>
2025-05-27 19:54:02 -0500

    compute added

---
+++ 7a - Compute.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "sample-vm" {
+  name         = "public-dallas-1"
+  machine_type = "e2-medium"
+  zone         = "us-south1"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.hqinternal.name
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = file("./startup.sh")
+
+}
---
+++ 7b - Template.tf
@@ -0,0 +1,26 @@
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_region_instance_template
+# https://developer.hashicorp.com/terraform/language/functions/file
+# Google Compute Engine: Regional Instance Template
+resource "google_compute_region_instance_template" "app" {
+  name         = "app-template-terraform"
+  description  = "App-Template"
+  machine_type = "e2-medium"
+
+  # Create a new disk from an image and set as boot disk
+  disk {
+    source_image = "debian-cloud/debian-12"
+    boot         = true
+  }
+
+  # Network Configurations 
+  network_interface {
+    subnetwork = google_compute_subnetwork.hqinternal.id
+    /*access_config {
+      # Include this section to give the VM an external IP address
+    } */
+  }
+
+  # Install Webserver using file() function
+  metadata_startup_script = file("./startup.sh")
+}
